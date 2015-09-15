6c6eff9762217775ea2f67d9deb43090e6feac0a
Hao CHEN <hao.ch86@gmail.com>
2015-09-15 20:43:45 +1200

    Updated commments

---
+++ compsci-335-blogger/src/main/java/communityblogger/services/CommentMapper.java
@@ -0,0 +1,29 @@
+package communityblogger.services;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import communityblogger.domain.Comment;
+
+public class CommentMapper {
+
+	static Comment toDomainModel(communityblogger.dto.Comment dtoComment) {
+		Comment fullComment = new Comment(dtoComment.getContent(),
+				dtoComment.getTimePosted());
+		return fullComment;
+	}
+
+	static communityblogger.dto.Comment toDto(Comment _comment) {
+		communityblogger.dto.Comment dtoComment = new communityblogger.dto.Comment(
+				_comment.getContent(), _comment.getTimePosted());
+		return dtoComment;
+	}
+
+	static Set<communityblogger.dto.Comment> toDto(Set<Comment> _comments) {
+		Set<communityblogger.dto.Comment> dtoComments = new HashSet<communityblogger.dto.Comment>();
+		for (Comment c : _comments) {
+			dtoComments.add(CommentMapper.toDto(c));
+		}
+		return dtoComments;
+	}
+}
